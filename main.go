75db3321678f73fbfca221307093788759892fd7
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-15 22:22:46 +0600

    Add random user agent selection to makeRequest function
    - Added a list of user agents and a function to select a random user agent from the list
    - Updated the makeRequest function to use the random user agent function for the User-Agent

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type SeoData struct{}
 type Parser interface{}
+
+// setting user agents
+var userAgents = []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+}
+
+func randomUserAgent() string {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Int() % len(userAgents)
+	fmt.Println(userAgents[randNum])
+	return userAgents[randNum]
+}
 
 // site map url extractor
 func extractSiteMapUrls() {
