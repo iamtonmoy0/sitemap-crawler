eb98b9715320b616dea9ebcd835e3284f17163f0
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-15 23:07:40 +0600

    "Add goquery as a dependency, modify SeoData struct and DefaultParser struct, update scrapeSitemap function to return a slice of SeoData, and print results in main function"

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"time"
 )
 
-type SeoData struct{}
+type SeoData struct {
+	URL             string
+	Title           string
+	H1              string
+	MetaDescription string
+	StatusCode      string
+}
+
 type Parser interface{}
+type DefaultParser struct{}
 
 // setting user agents
-var userAgents = []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
 	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
@@ -21,7 +30,6 @@ var userAgents = []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 func randomUserAgent() string {
 	rand.Seed(time.Now().Unix())
 	randNum := rand.Int() % len(userAgents)
-	fmt.Println(userAgents[randNum])
 	return userAgents[randNum]
 }
 
@@ -46,10 +54,14 @@ func crawlPage() {}
 func getSeoData() {}
 
 // scrape sitemap
-func scrapeSitemap() {
+func scrapeSitemap(url string) []SeoData {
 	scrapeUrl()
 	extractSiteMapUrls()
 }
 func main() {
-
+	p := DefaultParser{}
+	results := scrapeSitemap("")
+	for _, res := range results {
+		fmt.Println(res)
+	}
 }
