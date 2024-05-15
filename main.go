6d51959a61d83f2736423ef01ce0eab312196870
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-15 22:14:56 +0600

    Add function definitions for URL extraction, request making, scraping, and crawling; define interfaces for Parser and SeoData
    The changes include:
    
    - Definition of function extractSiteMapUrls() which makes a request

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+type SeoData struct{}
+type Parser interface{}
+
+// site map url extractor
+func extractSiteMapUrls() {
+	makeRequest()
+}
+
+// request maker
+func makeRequest() {}
+
+// scrape url
+func scrapeUrl() {}
+
+// scrape Page
+func scrapePage() {}
+
+// crawl page
+func crawlPage() {}
+
+// get seo data
+func getSeoData() {}
+
+// scrape sitemap
+func scrapeSitemap() {
+	scrapeUrl()
+	extractSiteMapUrls()
+}
 func main() {
-	
+
 }
